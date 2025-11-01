3de597b5f633e67ec600f9e06cad0c66fe3989b2
joinbrightnest <joinbrightnest@users.noreply.github.com>
2025-11-01 23:34:39 +0200

    Move mobile loading dots even higher to ensure full visibility

--- components/ResultIntroSequence.tsx
+++ components/ResultIntroSequence.tsx
@@ -130,8 +130,8 @@ export default function ResultIntroSequence({ name = "there", onComplete }: Resu
             )}
           </motion.div>
 
-          {/* Loading dots - mobile only - must be visible */}
-          <div className="absolute bottom-[100px] sm:bottom-20 left-1/2 -translate-x-1/2 flex space-x-4 sm:hidden z-[100] items-center pointer-events-none">
+          {/* Loading dots - mobile only - must be visible above progress indicators */}
+          <div className="absolute bottom-[140px] sm:bottom-20 left-1/2 -translate-x-1/2 flex space-x-4 sm:hidden z-[100] items-center pointer-events-none">
             {[0, 1, 2, 3].map((i) => (
               <motion.div
                 key={`dot-${i}-${index}`}
