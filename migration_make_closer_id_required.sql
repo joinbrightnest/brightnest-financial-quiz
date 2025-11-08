368924c5e0b8cda2a18f18483cac659c84cc87bf
joinbrightnest <joinbrightnest@users.noreply.github.com>
2025-11-08 19:21:41 +0200

    Clean up migration files after successful database update

--- migration_make_closer_id_required.sql
+++
@@ -1,9 +0,0 @@
--- Make closer_id required (NOT NULL) in tasks table
--- This ensures every task must be assigned to a closer
-
-ALTER TABLE "tasks"
-ALTER COLUMN "closer_id" SET NOT NULL;
-
--- Update the comment to reflect this is required
-COMMENT ON COLUMN "tasks"."closer_id" IS 'Required - every task must be assigned to a closer';
-
